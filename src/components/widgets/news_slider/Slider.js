c8a7ea0620ff88cf0ce074cf3b4ef06e3b83f545
guillaume <guillaume.peoch@gmail.com>
2018-07-27 12:48:33 -0700

    1 - Part 2 of the project - Firebase
    Implementing Firebase

--- src/components/widgets/news_slider/Slider.js
+++ src/components/widgets/news_slider/Slider.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 import SliderTemplates from './SliderTemplates';
-import { URLDev } from '../../../config';
+
+import { firebaseArticles, firebaseLooper } from '../../../firebase';
+
+// We are now using firebase
+// import { URLDev } from '../../../config';
 
 class Slider extends Component {
 
@@ -13,21 +17,37 @@ class Slider extends Component {
   }
 
   componentWillMount(){
-    fetch(`${URLDev}/articles`).then(function(response){
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-      return response.json();
-    }).then(myJson => {
-      const articles = myJson.splice(this.props.start,this.props.start+this.props.amount);
+
+    firebaseArticles.limitToFirst(3).once('value')
+    .then(function(snapshot){
+      return snapshot.val();
+    })
+    .then((data)=>{
+      const articles = firebaseLooper(data);
       this.setState({
         articles:articles
-      })
-    }).catch( function(error){
-      console.error(error);
-    });
+      });
+    })
+    .catch(function(e){
+      console.log(e);
+    })
+
+
+    // fetch(`${URLDev}/articles`).then(function(response){
+    //   if (response.status !== 200) {
+    //     console.log('Looks like there was a problem. Status Code: ' +
+    //       response.status);
+    //     return;
+    //   }
+    //   return response.json();
+    // }).then(myJson => {
+    //   const articles = myJson.splice(this.props.start,this.props.start+this.props.amount);
+    //   this.setState({
+    //     articles:articles
+    //   })
+    // }).catch( function(error){
+    //   console.error(error);
+    // });
   }
 
   render(){
---
+++ src/firebase.js
@@ -0,0 +1,38 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA_CsN6TAW4UBaWwz02lpsKCNZqKyfjyCw",
+    authDomain: "nba-app-fe94c.firebaseapp.com",
+    databaseURL: "https://nba-app-fe94c.firebaseio.com",
+    projectId: "nba-app-fe94c",
+    storageBucket: "nba-app-fe94c.appspot.com",
+    messagingSenderId: "592077906893"
+  };
+
+  firebase.initializeApp(config);
+
+  const firebaseLooper = function(thing){
+    const data = [];
+    for(let param in object){
+      data.push({
+          ...object[param],
+          id:param
+      });
+    }
+    return data;
+  }
+
+  const firebaseDB = firebase.database();
+
+  const firebaseArticles = firebaseDB.ref('articles');
+  const firebaseTeams = firebaseDB.ref('teams');
+  const firebaseVideos = firebaseDB.ref('videos');
+
+  export {
+    firebaseDB,
+    firebaseArticles,
+    firebaseTeams,
+    firebaseVideos,
+    firebaseLooper
+  }
