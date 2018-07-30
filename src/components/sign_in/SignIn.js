4c1b6a0b29acdb8d51533cf2a4eed278330d22b2
guillaume <guillaume.peoch@gmail.com>
2018-07-30 16:19:00 -0700

    Form fields
    Declaration of form fields

--- src/components/sign_in/SignIn.js
+++ src/components/sign_in/SignIn.js
@@ -1,16 +1,57 @@
 import React , { Component } from 'react';
+import FormField from '../widgets/form_fields/FormFields';
 import styles from './sign_in.css';
 
 class SignIn extends Component {
 
   state = {
-
+    registerError:'',
+    loading:false,
+    formdata:{
+      email:{
+        element:'input',
+        value:'',
+        config:{
+          name:'email',
+          type:'email',
+          placeHolder:'Enter your email'
+        },
+        validation:{
+          required:true,
+          email:true
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      },
+      password:{
+        element:'input',
+        value:'',
+        config:{
+          name:'password',
+          type:'password',
+          placeHolder:'Enter your password'
+        },
+        validation:{
+          required:true,
+          email:true
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      }
+    }
   }
 
   render(){
-    return (<div>
-      Sign In
-    </div>);
+    return (
+    <div className={styles.logContainer}>
+      <form>
+        <FormField formdata={this.state.formdata.email}/>
+        <FormField formdata={this.state.formdata.password}/>
+      </form>
+    </div>
+  );
   }
 
 }
---
+++ src/components/widgets/form_fields/FormFields.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './form_fields.css';
+
+const FormFields = function(props){
+  return (
+    <div>
+      FormFields
+    </div>
+  );
+}
+
+export default FormFields;
