e7ccb025ce192b3effcd49c6ffc5bff70b845d8e
guillaume <guillaume.peoch@gmail.com>
2018-07-31 19:12:44 -0700

    10 - finishing text editor and starting dropdown

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,210 @@
 import React , { Component } from 'react';
 import styles from './dashboard.css';
+import FormField from '../widgets/form_fields/FormFields';
+import { firebase } from '../../firebase';
+
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
+import { stateToHTML } from 'draft-js-export-html';
 
 class Dashboard extends Component {
   state = {
-    
+    editorState: EditorState.createEmpty(),
+    postError:'',
+    registerError:'',
+    loading:false,
+    formdata:{
+      title:{
+        element:'input',
+        value:'',
+        config:{
+          name:'title',
+          type:'title',
+          placeholder:'Enter the Title'
+        },
+        validation:{
+          required:true
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      },
+      author:{
+        element:'input',
+        value:'',
+        config:{
+          name:'author',
+          type:'author',
+          placeholder:'Enter your name'
+        },
+        validation:{
+          required:true,
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      },
+      body:{
+        element:'texteditor',
+        value:'',
+        valid:true
+      },
+      team:{
+        element:'select',
+        value:'',
+        config:{
+          options:[
+            {id:1,name:"OKC"},
+            {id:2,name:"SAS"}
+          ]
+        },
+        validation:{
+          required:true
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      }
+    }
   }
+
+  onEditorStateChange = (editorState) => {
+
+    let contentState = editorState.getCurrentContent();
+    let rawState = convertToRaw(contentState)
+
+    let html = stateToHTML(contentState)
+
+    this.updateForm({target:{value:html}},'body',false)
+
+    this.setState({
+        editorState
+    })
+}
+
+  updateForm = (e, id, blur) => {
+
+      const newFormData = {
+        ...this.state.formdata
+      }
+
+      const newElement = {
+        ...newFormData[id]
+      }
+
+      newElement.value = e.target.value;
+      newFormData[id]=newElement;
+
+      if(blur){
+        const error = this.validate(newElement);
+        newElement.valid = error[0];
+        newElement.validationMessage = error[1];
+      }
+
+      newElement.touched = blur;
+      newFormData[id] = newElement
+
+      this.setState({
+        formdata:newFormData
+      })
+  }
+
+  validate(element){
+    let error = [true,''];
+
+    if(element.validation.required){
+        const valid = element.value.trim() !== '';
+        const message = valid ? '':'This field is required';
+        error = valid ? error : [valid, message];
+    }
+
+
+    return error;
+  }
+
+  submitButton(){
+    return this.state.loading ?
+    'Loading...'
+    : (
+      <div>
+        <button onClick={(e)=>{this.submitForm(e,true)}}>Post Article</button>
+      </div>
+    );
+  }
+
+  submitForm = (e, type) => {
+    e.preventDefault();
+
+    if(type !== null ){
+      let valid = true;
+      let dataToSubmit = {};
+      for(let key in this.state.formdata){
+          valid = this.state.formdata[key].valid && valid;
+          dataToSubmit[key]=this.state.formdata[key].value;
+      }
+
+      console.log(dataToSubmit);
+
+      if(valid){
+        this.setState({
+          loading:true,
+          registerError:''
+        })
+        if(type){
+          // LOGIN
+
+        } else {
+          // REGISTER
+
+        }
+      } else {
+        console.log('Not valid!');
+      }
+    }
+  }
+
+
+  showError = () => (
+        this.state.postError !== '' ?
+            <div className={styles.error}>{this.state.postError}</div>
+        : ''
+    )
 
   render(){
     return(
-      <div>
-        Dashboard
+      <div className={styles.logContainer}>
+        <form onSubmit={(e)=>this.submitForm(e,null)}>
+
+          <h2>Post Article</h2>
+
+          <FormField
+            id='title'
+            formdata={this.state.formdata.title}
+            change={this.updateForm}
+            />
+
+          <FormField
+            id='author'
+            formdata={this.state.formdata.author}
+            change={this.updateForm}
+            />
+
+          <Editor
+            editorState={this.state.editorState}
+            wrapperClassName="myEditor-wrapper"
+            editorClassName="myEditor-editor"
+            onEditorStateChange={this.onEditorStateChange}
+            />
+
+            <FormField
+              id='team'
+              formdata={this.state.formdata.team}
+              change={this.updateForm}
+              />
+
+          { this.submitButton() }
+          { this.showError() }
+        </form>
       </div>
     );
   }
--- src/components/widgets/form_fields/FormFields.js
+++ src/components/widgets/form_fields/FormFields.js
@@ -3,6 +3,13 @@ import styles from './form_fields.css';
 
 const FormFields = function(props){
 
+  const getOptions = function(optionsArray){
+    const template = optionsArray.map(function(option){
+      return(<option value={option.id}>{option.name}</option>);
+    });
+    return template;
+  }
+
   const getMessage = function(){
     return !props.formdata.valid ?
       (
@@ -30,6 +37,21 @@ const FormFields = function(props){
             }}
           />
         </div>
+      );
+      break;
+    case 'select':
+      template = (
+        <select
+          value={props.formdata.value}
+          onChange={function(event){
+            return props.change(event,props.id,false)
+          }}
+          onBlur={function(event){
+            return props.change(event,props.id,true)
+          }}
+          className={styles.inputContainer}>
+          { getOptions(props.formdata.config.options) }
+      </select>
       );
       break;
     default:
